4541edd1e7fec44f1630cc536e4a37fadfa0397a
VivekPandey-07 <vp072926@gmail.com>
2026-07-20 23:38:59 +0530

    Arrays (largest & smallest) and nested loop patterns

---
+++ largest_element.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    cout<<"Enter elements: "<<endl;
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    int largest=arr[0];
+    for(int i=1;i<5;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    cout<<"Largest element: "<<largest;
+}
