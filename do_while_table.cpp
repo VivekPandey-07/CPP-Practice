b91fd7c89de7227f86e594b539a34bbca9bca667
VivekPandey-07 <vp072926@gmail.com>
2026-07-22 08:09:08 +0530

    While loop practice and do-while basics

---
+++ do_while_table.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    do{
+        cout<<"7"<<" *"<<i<<" ="<<7*i<<endl;
+        i++;
+    }
+    while(i<11);
+}
---
+++ while_sum_1_to_10.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1,sum=0;
+    while(i<11){
+        sum+=i;
+        i++;
+    }
+    cout<<"Sum is: "<<sum;
+} 
